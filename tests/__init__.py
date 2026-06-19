6e47f2c8375d32e6ca4be8e201288078d8b91510
vatsal220 <vatsal.p1997@gmail.com>
2026-06-19 15:14:24 -0400

    created testing directory

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""Tests
+Tests directory for developing and execution of unit, integration, and e2e tests.
+"""
---
+++ tests/integration/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""Integration Tests
+Integration tests directory for integration tests of the architecture design project.
+"""
